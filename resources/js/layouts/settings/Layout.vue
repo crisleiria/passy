035edac8ec12c5a9fd6926bde58f738addd105da
crisleiria <crisleiria@gmail.com>
2026-01-21 01:40:16 +0000

    - add several changes on passwords store - remove two factor auth and password login - add pin and master key logic - add users table fields

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -5,10 +5,8 @@ import { Separator } from '@/components/ui/separator';
 import { toUrl, urlIsActive } from '@/lib/utils';
 import { edit as editAppearance } from '@/routes/appearance';
 import { edit as passkeys } from '@/routes/passkeys';
-import { edit as editPassword } from '@/routes/password';
 import { edit as editPin } from '@/routes/pin';
 import { edit as editProfile } from '@/routes/profile';
-import { show } from '@/routes/two-factor';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
 
@@ -17,18 +15,10 @@ const sidebarNavItems: NavItem[] = [
         title: 'Profile',
         href: editProfile(),
     },
-    // {
-    //     title: 'Password',
-    //     href: editPassword(),
-    // },
     {
         title: 'PIN',
         href: editPin(),
     },
-    // {
-    //     title: 'Two-Factor Auth',
-    //     href: show(),
-    // },
     {
         title: 'PassKeys',
         href: passkeys(),
